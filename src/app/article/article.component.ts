d99129a18fc48b9384690be7a1b761ba021efbd4
nobody special <nobody@nowhere.com>
2017-01-17 07:17:12 -0800

    added article class

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
+
 export class ArticleComponent implements OnInit {
+    @Input() article: Article;
 
-  constructor() { }
-
+  votes: number;
+  title: string;
+  link: string;
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
   ngOnInit() {
   }
-
 }
